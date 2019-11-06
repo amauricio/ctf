41e5622dd895f057912cf11de607ada447e4637b
Mauricio <uaxp123@gmail.com>
2019-11-06 03:59:08 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, session, request,jsonify, make_response,render_template
 import json
+Import base64
 app = Flask(__name__)
 
 @app.route("/")
@@ -9,7 +10,13 @@ def index():
     resp=make_response(render_template("index.html"))
     resp.set_cookie('ctf.flag', fg)
     if request.method == "DELETE":
-        
+        if "ctf.flag" in request.cookies:
+            try:
+                js = json.loads(base64.b64decode(request.cookies["ctf.flag"]))
+                if js["user"] == "admin":
+                    return "flag{flag_ctf_527287353}"
+            except:
+                return "Bad cookie"
     return resp
 if __name__ =="__main__":
     app.run(port=80, host="0.0.0.0")
