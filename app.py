2d6aa5e28ef12ff5088dc3675aa087de63e0f872
Mauricio <uaxp123@gmail.com>
2019-11-06 03:43:05 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, session, request, render$
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    Resp=make_response(render_template("index.html"))
+    resp.set_cookie('somecookiename', 'I am cooki')
+    return resp
+if __name__ =="__main__":
+    app.run(port=80, host="0.0.0.0")
