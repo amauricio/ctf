0d478a87b8d2ea0040d0fc0cb580e75737f9ac43
Mauricio <uaxp123@gmail.com>
2019-11-06 03:52:35 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask, session, request, render_template
+from flask import Flask, session, request,jsonify, make_response,render_template
+import json
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    Resp=make_response(render_template("index.html"))
-    resp.set_cookie('somecookiename', 'I am cooki')
+    data = jsonify({"user":"guest", "message":"hacker easy"})
+    fg = base64.b64encode(data.encode("utf-8"))
+    resp=make_response(render_template("index.html"))
+    resp.set_cookie('ctf.flag', fg)
+    if request.method == "DELETE":
+        
     return resp
 if __name__ =="__main__":
     app.run(port=80, host="0.0.0.0")
